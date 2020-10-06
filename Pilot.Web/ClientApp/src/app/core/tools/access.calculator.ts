376035b215b25a10e142f94f2071c59b352bb11f
Pavel Dmitriev <dmitriev_pe@ascon.ru>
2020-10-06 12:41:50 +0300

    AccessCalculator tests

--- Pilot.Web/ClientApp/src/app/core/tools/access.calculator.ts
+++ Pilot.Web/ClientApp/src/app/core/tools/access.calculator.ts
@@ -16,18 +16,17 @@ export class AccessCalculator {
     }
 
     calcAccessForPerson(node: IObject, person: IPerson) : AccessLevel {
-        var currentPerson = person;
-        if (!currentPerson)
+        if (!person)
             return AccessLevel.None;
 
-        var access = this.objectSecurity.getAccessByPerson(node, currentPerson);
+        let access = this.objectSecurity.getAccessByPerson(node, person);
 
         if(node.stateInfo.state == ObjectState.InRecycleBin)
             access-= AccessLevel.Edit;
 
         if (node.stateInfo.state == ObjectState.Frozen)
         {
-            var activeAccess = AccessLevel.None;
+            let activeAccess = AccessLevel.None;
             if ((access & AccessLevel.View) === AccessLevel.View)
                 activeAccess = AccessLevel.View;
             if ((access & AccessLevel.Freeze) === AccessLevel.Freeze)
