44b9003f8506134b34f9844cfdc224d4e3b7c573
Pavel Dmitriev <dmitriev_pe@ascon.ru>
2020-12-26 21:24:22 +0300

    fix FilesControllerTests

--- Pilot.Web.Tests/FilesControllerTests.cs
+++ Pilot.Web.Tests/FilesControllerTests.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Ascon.Pilot.DataClasses;
+using Ascon.Pilot.DataModifier;
 using Castle.Core.Internal;
 using DocumentRender;
 using Microsoft.AspNetCore.Http;
