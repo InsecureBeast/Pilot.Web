3b463d837005831641fa49645823daba89db7182
Pavel Dmitriev <isr3@mail.ru>
2021-08-20 22:52:09 +0300

    fixed offset

--- Pilot.Web/ClientApp/src/app/documents/components/document-viewer/viewer-page/remark-loaded.directive.ts
+++ Pilot.Web/ClientApp/src/app/documents/components/document-viewer/viewer-page/remark-loaded.directive.ts
@@ -11,15 +11,18 @@ export class RemarkLoadedDirective implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit(): void {
-    
-    let path = document.getElementById(this.appRemarkLoaded.remark.id + 'path');
-    this.appRemarkLoaded.boundRect = path.getBoundingClientRect();
 
-    let div = document.getElementById(this.appRemarkLoaded.remark.id + '-clickArea');
-    div.style.width = `${this.appRemarkLoaded.boundRect.width + 10}px`;
-    div.style.height = `${this.appRemarkLoaded.boundRect.height + 10}px`;
-    div.style.top = `${this.appRemarkLoaded.position.top - 5}px`;
-    div.style.left = `${this.appRemarkLoaded.position.left - 5}px`;
-    div.style.marginBottom = `-${(this.appRemarkLoaded.boundRect.height + 10)}px`;
+    if (!this.appRemarkLoaded.remark.text) {
+      let path = document.getElementById(this.appRemarkLoaded.remark.id + 'path');
+      this.appRemarkLoaded.boundRect = path.getBoundingClientRect();
+  
+      let div = document.getElementById(this.appRemarkLoaded.remark.id + '-clickArea');
+      div.style.width = `${this.appRemarkLoaded.boundRect.width + 10}px`;
+      div.style.height = `${this.appRemarkLoaded.boundRect.height + 10}px`;
+      div.style.top = `${this.appRemarkLoaded.position.top - 5}px`;
+      div.style.left = `${this.appRemarkLoaded.position.left - 5}px`;
+      div.style.marginBottom = `-${(this.appRemarkLoaded.boundRect.height + 10)}px`;
+      //div.style.marginLeft = `-${(this.appRemarkLoaded.boundRect.width + 10)}px`;
+    }
   }
 }
