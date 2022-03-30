08e28450f323215cb2f1ee6cb53faa9c1b376de8
Pavel Dmitriev <isr3@mail.ru>
2022-03-30 19:43:41 +0300

    Fixed displaying numerator template with repeated format term

--- Pilot.Web/Tools/Numerator/CurrentDateProvider.cs
+++ Pilot.Web/Tools/Numerator/CurrentDateProvider.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using Ascon.Pilot.DataClasses;
 
 namespace Pilot.Web.Tools.Numerator
 {
-    public class CurrentDateProvider : INumeratorKeywordProvider
+    public class CurrentDateProvider : INumeratorKeywordProvider<INObject>
     {
         public const string CURRENT_DATE_KEYWORD = "CurrentDate";
         private readonly bool _deferred;
@@ -19,6 +20,11 @@ namespace Pilot.Web.Tools.Numerator
                 return null;
 
             return _deferred ? (object)"***" : DateTime.Now;
+        }
+
+        public IEnumerable<object> GetValues(INObject obj, string keyword)
+        {
+            return new[] { GetValue(obj, keyword) };
         }
     }
 }
---
+++ Pilot.Web/Tools/Numerator/NumeratorMultiValueFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ascon.Pilot.DataClasses;
+
+namespace Pilot.Web.Tools.Numerator
+{
+    public class NumeratorMultiValueFormatter<T> : NumeratorFormatter<T>
+    {
+        public NumeratorMultiValueFormatter(INumeratorKeywordProvider<T> keywordProvider, Dictionary<string, IArgumentTemplate> templates = null, IContextFactory<T> contextFactory = null) : base(keywordProvider, templates, contextFactory)
+        {
+        }
+
+        protected override string GetFormattedArguments(T obj, string keyword, string separator, string itemFormat, TemplateFormatter<T> templateFormatter)
+        {
+            var values = _keywordProvider.GetValues(obj, keyword).ToList();
+            return templateFormatter.Format(values, separator, itemFormat);
+        }
+    }
+}
